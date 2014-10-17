45daad2e099ae6daab089a1232ffeda6d83d10ce
naruepat payachai <naruepat.p@gmail.com>
2014-10-17 13:27:41 +0700

    edit default config

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,6 @@
 <?php
-
 return array(
-
-	'domain' => 'http://localhost/service.wheretothailand.com/public',
-	'client_id' => '264dfc205a730639a42f595520bc4bdf',
-	'client_secret' => 'b309d59c68f105b80d43897ef4fbc28d'
-
+	'domain' => 'http://www.example.com', //Service Base URL
+	'client_id' => '264dfc205a730639a42f595520bc4bdf', //Client ID
+	'client_secret' => 'b309d59c68f105b80d43897ef4fbc28d' //Client Secret
 	);
